3e49d6f4994f3075e6acb5666e255c8d04295481
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2024-06-19 16:02:11 +0800

    index.ts

--- src/arrays/index.ts
+++ src/arrays/index.ts
@@ -1,5 +1,4 @@
 import Person from "../data/people/types";
-
 /* All functions here accept the peopleArray as an argument
  the peopleArray is an array of Person objects
  the person object has multiple properties, such as name and age
@@ -24,19 +23,21 @@ function getPeopleAbove30(peopleArray: Person[]): Person[] {
   // Example :
   // before: [{ name: 'Alice', age: 35 }, { name: 'Bob', age: 40 }, { name: 'Charlie', age: 20 }, { name: 'David', age: 30 }]
   // after: [{ name: 'Alice', age: 35 }, { name: 'Bob', age: 40 }]
+  const adultsArray = peopleArray.filter((Person) => Person.age > 30);
 
-  return [];
+
+  return (adultsArray);
 }
 
 // Function to demonstrate the find() method
 // MDN reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
-function findFirstAbove30(peopleArray: Person[]): Person | undefined {
+function findFirstAbove30(peopleArray: Person[]): Person|undefined {
   // e.g. const person = peopleArray.find(callback);
   // Goal: return the first Person object whose age is greater than 30 and city is 'Kuala Lumpur'
   // return type: Person
   // Example: { name: 'Bob', age: 40, city: 'Kuala Lumpur' }
-
-  return { name: "", age: 0, city: "", gender: "" };
+  const first = peopleArray.find((Person) => Person.age > 30 && Person.city === 'Kuala Lumpur');
+  return first;
 }
 
 // Function to demonstrate the some() method
@@ -45,8 +46,10 @@ function getIsSomeAdults(peopleArray: Person[]): boolean {
   // e.g. const isAdult = peopleArray.some(callback);
   // Goal: return a boolean value indicating whether there is at least one Person object whose age is greater than or equal to 18
   // return type: boolean
-
-  return false;
+  const ofAge = (Person:any) => (Person.age >= 18);
+  const result = (peopleArray.some(ofAge));
+console.log(result)
+  return result;
 }
 
 // Function to demonstrate the every() method
@@ -55,8 +58,9 @@ function getIsAllAdults(peopleArray: Person[]): boolean {
   // e.g. const allAdults = peopleArray.every(callback);
   // Goal: return a boolean value indicating whether all Person objects have an age greater than 30
   // return type: boolean
-
-  return false;
+  const ofAge = (Person:any) => (Person.age >= 18);
+  const result = (peopleArray.some(ofAge));
+  return result;
 }
 
 // Function to demonstrate the slice() method
@@ -65,7 +69,7 @@ function getPersons1to3(peopleArray: Person[]): Person[] {
   // e.g. const slicedArray = peopleArray.slice(start, end);
   // Goal: return a new array of Person objects starting from index 1 to 3
   // return type: Person[]
-  return [];
+  return peopleArray.slice(0,3);
 }
 
 // Function to demonstrate the sort() method
@@ -74,7 +78,7 @@ function sortByAge(peopleArray: Person[]): Person[] {
   // e.g. const sortedArray = peopleArray.sort(callback);
   // Goal: return a new array of Person objects sorted by age in ascending order
   // return type: Person[]
-  return [];
+  return peopleArray.sort()//((a,b) => a.age - b.age);
 }
 
 export {
